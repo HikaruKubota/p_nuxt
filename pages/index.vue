df307de606830513edba4c4c22bfccea7fffff3e
sakaku193 <sakaku193@gmail.com>
2020-08-21 11:43:19 +0900

    nuxt1

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,47 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        first2
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <pre>[{{now}}]</pre>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {
+  data: function () {
+    return {
+      title: 'hello!',
+      message: 'this is a message!',
+      now: 'wait...'
+    }
+  },
+  created: function () {
+    setInterval(()=>{
+      let a = new Date()
+      this.now = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds()
+    }, 1000)
+  },
+}
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 5px 10px;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+h1 {
+  font-size: 60pt;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+p {
+  padding: 5px;
+  font-size: 20pt;
 }
-
-.links {
-  padding-top: 15px;
+pre {
+  padding: 10px;
+  font-size: 20pt;
+  background-color: #efefef;
+}
+hr {
+  margin: 10px 0;
 }
 </style>
